9cfb54fe9f2ea37aa1c4c19eab4ed86d2def33f5
wiygdiwgw <alikhanchik1582@gmail.com>
2024-02-08 21:21:05 +0600

    finally bitch

--- src/components/homepage/Navbar.jsx
+++ src/components/homepage/Navbar.jsx
@@ -3,38 +3,39 @@ import styles from "./css/Navbar.module.css";
 import { IoBookmarkOutline } from "react-icons/io5";
 import { RxAvatar } from "react-icons/rx";
 import { FaEye } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.left}>
         <div className={styles.logo}>
-          <a className={styles.logo_1} href="#">
+          <Link className={styles.logoLink} to="/">
             MALIK PIDR
-          </a>
+          </Link>
         </div>
       </div>
       <div className={styles.right}>
-        <div className={styles.acc_block}>
-          <div className={styles.search_block}>
+        <div className={styles.accBlock}>
+          <div className={styles.searchBlock}>
+            {/* Keep search functionality intact */}
             <input
-              className={styles.search_block_1}
+              className={styles.searchInput}
               type="text"
               placeholder="Search..."
             />
           </div>
-          <div className={styles.seperator}></div>
-          <div className={styles.fav_block}>
-            <IoBookmarkOutline className={styles.fav_block_1} />
+          <div className={styles.iconBlock}>
+            <IoBookmarkOutline className={styles.icon} />
           </div>
-          <div className={styles.watched_block}>
-            <FaEye className={styles.watched_block_1} />
+          <div className={styles.iconBlock}>
+            <FaEye className={styles.icon} />
           </div>
           <div className={styles.login}>
-            <button className={styles.login_1}>
-              <RxAvatar className={styles.login_2} />
+            <Link to="/auth" className={styles.loginLink}>
+              <RxAvatar className={styles.avatarIcon} />
               Войти
-            </button>
+            </Link>
           </div>
         </div>
       </div>
