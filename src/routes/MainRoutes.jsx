2235c9989e5335b4621779200972399c54333eec
wiygdiwgw <alikhanchik1582@gmail.com>
2024-02-09 20:19:17 +0600

    m1

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,32 +1,31 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-
-import HomePage from '../pages/HomePage'
-import AuthPage from '../pages/AuthPage'
-import AddProduct from '../components/product/AddProduct'
-import AddGenre from '../components/product/AddGenre'
-import ProductPage from '../pages/ProductPage'
-import EditPage from './../pages/EditPage'
-import Favorite from '../components/contextPage/Favorite'
-import Viewed from '../components/contextPage/Viewed'
-import Login from '../components/Malik/Login'
-import Register from '../components/Malik/Register'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import AuthPage from "../pages/AuthPage";
+import AddProduct from "../components/product/AddProduct";
+import AddGenre from "../components/product/AddGenre";
+import ProductPage from "../pages/ProductPage";
+import EditPage from "./../pages/EditPage";
+import Login from "../components/Malik/Login";
+import Register from "../components/Malik/Register";
+import Favorite from "../components/Malik/Favorite";
+import Viewed from "../components/Malik/Viewed";
 
 const MainRoutes = () => {
-	return (
-		<Routes>
-			<Route path='/' element={<HomePage />} />
-			<Route path='/products' element={<ProductPage />} />
-			<Route path='/fav' element={<Favorite />} />
-			<Route path='/viewed' element={<Viewed />} />
-			<Route path='/edit/:id' element={<EditPage />} />
-			<Route path='/addProduct' element={<AddProduct />} />
-			<Route path='/addCategory' element={<AddGenre />} />
-			<Route path='/auth' element={<AuthPage />} />
-			<Route path='/login' element={<Login />} />
-			<Route path='/register' element={<Register />} />
-		</Routes>
-	)
-}
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/products" element={<ProductPage />} />
+      <Route path="/fav" element={<Favorite />} />
+      <Route path="/viewed" element={<Viewed />} />
+      <Route path="/edit/:id" element={<EditPage />} />
+      <Route path="/addProduct" element={<AddProduct />} />
+      <Route path="/addCategory" element={<AddGenre />} />
+      <Route path="/auth" element={<AuthPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
+};
 
-export default MainRoutes
+export default MainRoutes;
