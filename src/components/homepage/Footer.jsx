3036b22a0bb9dfe3433bc00eac764dcce6519293
kuji <146005118+kujijjk@users.noreply.github.com>
2024-02-13 18:41:34 +0600

    m5

--- src/components/homepage/Footer.jsx
+++ src/components/homepage/Footer.jsx
@@ -1,53 +1,53 @@
-import React from "react";
-import styles from "./css/Footer.module.css";
+import React from 'react'
+import styles from './css/Footer.module.css'
 
 const Footer = () => {
-  return (
-    <footer className={styles.footer}>
-      <div className={styles.inner}>
-        <div className={styles.column}>
-          <h3>Malik Pidr</h3>
-          <ul>
-            <li>
-              <a href="#">О проекте</a>
-            </li>
-            <li>
-              <a href="#">Поддержать проект</a>
-            </li>
-            <li>
-              <a href="#">Заказать фильм</a>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.column}>
-          <h3>Вопросы и ответы (FAQ)</h3>
-          <ul>
-            <li>
-              <a href="#">Служба поддержки</a>
-            </li>
-            <li>
-              <a href="#">Чат Telegram</a>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.column}>
-          <h3>Заявка на удаление контента</h3>
-          <ul>
-            <li>
-              <a href="#">Правообладателям</a>
-            </li>
-            <li>
-              <a href="#">Пользовательское соглашение</a>
-            </li>
-            <li>
-              <a href="#">Политика конфиденциальности</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <p>&copy; 2024 Malik Pidr</p>
-    </footer>
-  );
-};
+	return (
+		<footer className={styles.footer}>
+			<div className={styles.inner}>
+				<div className={styles.column}>
+					<h3>CINEMA ONLINE</h3>
+					<ul>
+						<li>
+							<a href='#'>О проекте</a>
+						</li>
+						<li>
+							<a href='#'>Поддержать проект</a>
+						</li>
+						<li>
+							<a href='#'>Заказать фильм</a>
+						</li>
+					</ul>
+				</div>
+				<div className={styles.column}>
+					<h3>Вопросы и ответы (FAQ)</h3>
+					<ul>
+						<li>
+							<a href='#'>Служба поддержки</a>
+						</li>
+						<li>
+							<a href='#'>Чат Telegram</a>
+						</li>
+					</ul>
+				</div>
+				<div className={styles.column}>
+					<h3>Заявка на удаление контента</h3>
+					<ul>
+						<li>
+							<a href='#'>Правообладателям</a>
+						</li>
+						<li>
+							<a href='#'>Пользовательское соглашение</a>
+						</li>
+						<li>
+							<a href='#'>Политика конфиденциальности</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<p>&copy; 2024 CINEMA ONLINE</p>
+		</footer>
+	)
+}
 
-export default Footer;
+export default Footer
--- src/components/homepage/Navbar.jsx
+++ src/components/homepage/Navbar.jsx
@@ -1,46 +1,46 @@
-import React from "react";
-import styles from "./css/Navbar.module.css";
-import { IoBookmarkOutline } from "react-icons/io5";
-import { RxAvatar } from "react-icons/rx";
-import { FaEye } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import React from 'react'
+import styles from './css/Navbar.module.css'
+import { IoBookmarkOutline } from 'react-icons/io5'
+import { RxAvatar } from 'react-icons/rx'
+import { FaEye } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.left}>
-        <div className={styles.logo}>
-          <Link className={styles.logoLink} to="/">
-            MALIK PIDR
-          </Link>
-        </div>
-      </div>
-      <div className={styles.right}>
-        <div className={styles.accBlock}>
-          <div className={styles.searchBlock}>
-            {/* Keep search functionality intact */}
-            <input
-              className={styles.searchInput}
-              type="text"
-              placeholder="Search..."
-            />
-          </div>
-          <div className={styles.iconBlock}>
-            <IoBookmarkOutline className={styles.icon} />
-          </div>
-          <div className={styles.iconBlock}>
-            <FaEye className={styles.icon} />
-          </div>
-          <div className={styles.login}>
-            <Link to="/auth" className={styles.loginLink}>
-              <RxAvatar className={styles.avatarIcon} />
-              Войти
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+	return (
+		<div className={styles.container}>
+			<div className={styles.left}>
+				<div className={styles.logo}>
+					<Link className={styles.logoLink} to='/'>
+						CINEMA ONLINE
+					</Link>
+				</div>
+			</div>
+			<div className={styles.right}>
+				<div className={styles.accBlock}>
+					<div className={styles.searchBlock}>
+						{/* Keep search functionality intact */}
+						<input
+							className={styles.searchInput}
+							type='text'
+							placeholder='Search...'
+						/>
+					</div>
+					<div className={styles.iconBlock}>
+						<IoBookmarkOutline className={styles.icon} />
+					</div>
+					<div className={styles.iconBlock}>
+						<FaEye className={styles.icon} />
+					</div>
+					<div className={styles.login}>
+						<Link to='/auth' className={styles.loginLink}>
+							<RxAvatar className={styles.avatarIcon} />
+							Войти
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
 
-export default Navbar;
+export default Navbar
